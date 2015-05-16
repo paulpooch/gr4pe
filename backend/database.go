7e75fc86b001bee00debb9784d6db4a32a5dc5fc
Paul Pucciarelli <paul@ticketevolution.com>
2015-05-16 17:16:56 -0400

    Convert to Gin.  Dump Gorilla.

--- backend/database.go
+++ backend/database.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"database/sql"
+	"log"
 	"fmt"
 	_ "github.com/lib/pq"
-	"log"
 )
 
 const DB_NAME = "gr4pe"
 const DB_USERNAME = "gr4pe-user"
 const SLL_MODE = "disable"
 
-// http://blog.golang.org/defer-panic-and-recover
 // http://go-database-sql.org/accessing.html
 // http://astaxie.gitbooks.io/build-web-application-with-golang/content/en/05.4.html
 
 func SetupDB() *sql.DB {
 	config := fmt.Sprintf("dbname=%s user=%s sslmode=%s", DB_NAME, DB_USERNAME, SLL_MODE)
 
-	// lazy open - won't open until first query is made
+	// Lazy open - won't open until first query is made.
 	db, err := sql.Open("postgres", config)
-
-	if err != nil {
-		log.Fatal(err)
+	if (err != nil) {
+		log.Print("sql.Open failed.")
 		panic(err)
 	}
 
+	// Try to hit db to make sure it's up.
+	pingError := db.Ping()
+	if (pingError != nil) {
+		log.Print("Database test connection failed.")
+		panic(pingError)
+	}
 	return db
 }
 
@@ -38,26 +42,3 @@ created_at TIMESTAMP DEFAULT now() NOT NULL,
 is_logged_in BOOLEAN DEFAULT FALSE  NOT NULL,
 email VARCHAR NOT NULL
 */
-func AccountRegister(db *sql.DB) {
-
-	username := "paul"
-	pwHash := "a"
-	pwSalt := "b"
-	email := "paul@paul.com"
-
-	var lastInsertId int
-	var err = db.QueryRow(`INSERT INTO accounts(
-		username,
-		password_hash,
-		password_salt,
-		email
-	) VALUES($1, $2, $3, $4) returning id;`, username, pwHash, pwSalt, email).Scan(&lastInsertId)
-
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-
-	fmt.Println("last inserted id =", lastInsertId)
-
-}
--- backend/main.go
+++ backend/main.go
@@ -2,31 +2,77 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
+
+const DEV_PORT = ":8080"
+const PROD_PORT = ":80"
 
 var Production = flag.Bool("production", false, "Use the -production flag to run in production mode")
 
+type RegisterLoginForm struct {
+	Email			string	`form:"inputEmail" 		binding:"required"`
+	Password	string 	`form:"inputPassword" binding:"required"`
+}
+
 func main() {
+
 	flag.Parse()
+	db := SetupDB()
+	r := gin.Default()
 
-	mode := "Development"
-	port := "8080"
+	register := func(c *gin.Context) {
+		var form RegisterLoginForm
+		c.BindWith(&form, binding.MultipartForm)
 
-	if *Production {
-		mode = "Production"
-		port = "8080"
+		username := form.Email
+		pwHash := "abc"
+		pwSalt := "123"
+		email := form.Email
+
+		var lastInsertId int
+		var err = db.QueryRow(`INSERT INTO accounts(
+			username,
+			password_hash,
+			password_salt,
+			email
+		) VALUES($1, $2, $3, $4) returning id;`, username, pwHash, pwSalt, email).Scan(&lastInsertId)
+
+		if err != nil {
+			log.Fatal(err)
+			panic(err)
+		}
+
+		fmt.Println("last inserted id =", lastInsertId)
+
+		c.JSON(200, gin.H{"status": lastInsertId })
+		//c.JSON(401, gin.H{"status": "unauthorized"})
 	}
 
-	log.Printf("Server running in %s mode on port %s", mode, port)
+	login := func(c *gin.Context) {
+		var form RegisterLoginForm
+		c.BindWith(&form, binding.MultipartForm)
+		c.JSON(200, gin.H{"status": `Hi ${ form.Email }`})
+		//c.JSON(401, gin.H{"status": "unauthorized"})
+	}
 
-	router := NewRouter()
+	api := r.Group("/api")
+	{
+		users := api.Group("/users")
+		{
+			users.POST("/register", register)
+			users.POST("/login", 		login)
+		}
+	}
 
-	database := SetupDB()
-	AccountRegister(database)
-
-	http.Handle("/", router)
-
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	// Listen and server on 0.0.0.0:8080
+	port := DEV_PORT
+	if *Production {
+		port = PROD_PORT
+	}
+	r.Run(port)
 }
